b059ada20541c4cf301980ab7bb8002784ab3a50
KirillPlaksin1 <montanachel@gmail.com>
2023-10-29 14:43:15 +0700

    first commit

---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 33",
+    "rentTime": 5,
+    "deliveryDate": "2023-10-30",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
